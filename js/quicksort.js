01f1fb22ac14843b72693980070b945be48648cc
siigdev <sebbahlol@gmail.com>
2018-12-25 17:10:11 +0100

    quicksort implementation

---
+++ js/quicksort.js
@@ -0,0 +1,36 @@
+// QUICK SORT
+// https://en.wikipedia.org/wiki/Quicksort
+// Worst case: O(N^2)
+// Best case: O(n log n)
+// Avg case: O(n log n)
+
+function quickSort(valuesArray, low, high) {
+    var pivot;
+    var partitionvalue;
+    if (low < high){
+        pivot = high;
+        partitionvalue = partition(valuesArray, pivot, low, high);
+        quickSort(valuesArray, low, partitionvalue-1);
+        quickSort(valuesArray, partitionvalue+1, high);
+    }
+    return valuesArray;
+}
+
+function partition(valuesArray, pivot, low, high){
+    var pivotvalue = valuesArray[pivot];
+    var partitionvalue = low;
+
+    for (var i = low; i < high; i++){
+        if(valuesArray[i] < pivotvalue) {
+            let a = valuesArray[i];
+            valuesArray[i] = valuesArray[partitionvalue];
+            valuesArray[partitionvalue] = a;
+            partitionvalue++;
+        }
+    }
+    let b = high;
+    high = valuesArray[partitionvalue];
+    valuesArray[partitionvalue] = b;
+    
+    return partitionvalue;
+}
